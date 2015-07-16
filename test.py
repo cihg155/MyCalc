47b85bad812851f055fd5965ddfce43ebf74121f
snag bae Yun <yundream@gmail.com>
2015-07-16 15:04:35 +0900

    First commit

---
+++ test.py
@@ -0,0 +1,29 @@
+import unittest
+
+class Calc:
+    def sum(self, a, b):
+        return a+b
+    def divide(self, a, b):
+        return a/b
+    def sub(self, a, b): 
+        return a - b
+    def multi(self, a, b):
+        return a * b
+
+class TestStringMethods(unittest.TestCase):
+    def test_sum(self):
+        c = Calc()
+        self.assertEqual(c.sum(7,8), 15)
+    def test_sub(self):
+        c = Calc()
+        self.assertEqual(c.sub(7,8), -1)
+    def test_divide(self):
+        c = Calc()
+        self.assertEqual(c.divide(7,8), 0.88)
+        self.assertEqual(c.divide(7,0), 0.88)
+    def test_multi(self):
+        c = Calc()
+        self.assertEqual(c.multi(-8.127,2), -16.254)
+
+if __name__ == '__main__':
+    unittest.main()
